e1337c3e11526447c5355eb4a1615a39d105e5f3
Paulatang <111556773+Paulatang@users.noreply.github.com>
2022-10-14 12:46:58 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,6 @@
 var vg_1 = "map.json"; 
 var vg_2 = "ownershiptype.json"; 
+var vg_3 = "Country_store.json"
 
 vegaEmbed("#map", vg_1).then(function(result) {
     
@@ -8,6 +9,12 @@ vegaEmbed("#map", vg_1).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#radial_plot", vg_2).then(function(result) {
+    
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+vegaEmbed("#bar_chart_labels", vg_3).then(function(result) {
     
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
